f83411758a5742d1f42a2a8450e9c9694adf1b74
diathekez <96537263+diathekez@users.noreply.github.com>
2022-12-01 20:42:35 -0500

    Fizzbuzz solution added.

---
+++ November 2022/Fizzbuzz-Medium.js
@@ -0,0 +1,19 @@
+// Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”.
+
+// Source:
+
+// https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
+
+function fizzBuzz(num) {
+  for (let i = 1; 1 <= num; i++) {
+    if (i % 6 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 2 === 0) {
+      console.log("Fizz");
+    } else if (i % 3 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
