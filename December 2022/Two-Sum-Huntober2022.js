ddf06417f698f73599330d1d54dc10b59f7fcf88
diathekez <96537263+diathekez@users.noreply.github.com>
2022-12-15 18:32:25 -0500

    December 15 solution.

---
+++ December 2022/Two-Sum-Huntober2022.js
@@ -0,0 +1,25 @@
+// Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
+
+// describe("Two Sum", () => {
+//  it("Should implement two sum", () => {
+//   assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]);
+//  });
+// });
+
+// Source:
+// https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
+
+function twoSum(arr, sum) {
+  const pairs = [];
+  const nums = {};
+
+  for (const num1 of arr) {
+    const num2 = sum - num1;
+    if (nums[num2]) {
+      pairs.push([num1, num2]);
+    } else {
+      nums[num1] = 1;
+    }
+  }
+  return pairs;
+}
