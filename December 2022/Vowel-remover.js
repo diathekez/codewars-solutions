611a2c2b32c0bab54b5b64f84a83c85fd1993496
diathekez <96537263+diathekez@users.noreply.github.com>
2022-12-22 23:25:53 -0500

    December 22 Codewars solution.

---
+++ December 2022/Vowel-remover.js
@@ -0,0 +1,28 @@
+// Vowel remover
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+
+// don't worry about uppercase vowels
+// y is not considered a vowel for this kata
+
+// Source:
+// https://www.codewars.com/kata/5547929140907378f9000039/train/javascript
+
+function shortcut(string) {
+  return string.split(/[aeiou]/g).join("");
+}
+
+// Solution without regex
+
+// function shortcut (string) {
+//     return string
+//       .split('')
+//       .filter(str => !'aeiou'.includes(str))
+//       .join('')
+//   }
